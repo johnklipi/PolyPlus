82c50a4f73993254c4ba5afc56007366925e18a9
unknown <gpluiy@gmail.com>
2025-01-10 20:30:12 +0100

    Moved luxidoor capital back to level one, but giving it park from the beginning. Attempt to fix waterembark

--- PolyPlusPatcher.cs
+++ PolyPlusPatcher.cs
@@ -20,12 +20,19 @@ namespace PolyPlus {
 
         internal static void CreateEnumCaches()
         {
+            Console.Write("Created mapping for PlayerAbility.Type with id waterembark and index " + PolyMod.ModLoader.autoidx);
             EnumCache<PlayerAbility.Type>.AddMapping("waterembark", (PlayerAbility.Type)PolyMod.ModLoader.autoidx);
 			EnumCache<PlayerAbility.Type>.AddMapping("waterembark", (PlayerAbility.Type)PolyMod.ModLoader.autoidx);
             PolyMod.ModLoader.autoidx++;
+            Console.Write("Created mapping for PlayerAbility.Type with id polyplusstatic and index " + PolyMod.ModLoader.autoidx);
             EnumCache<UnitAbility.Type>.AddMapping("polyplusstatic", (UnitAbility.Type)PolyMod.ModLoader.autoidx);
 			EnumCache<UnitAbility.Type>.AddMapping("polyplusstatic", (UnitAbility.Type)PolyMod.ModLoader.autoidx);
             PolyMod.ModLoader.autoidx++;
+            int tribeBonusAutoidx = (int)Enum.GetValues(typeof(TribeData.BonusEnum)).Cast<TribeData.BonusEnum>().Last();
+            tribeBonusAutoidx++;
+            Console.Write("Created mapping for TribeData.BonusEnum with id citypark and index " + tribeBonusAutoidx);
+            EnumCache<TribeData.BonusEnum>.AddMapping("citypark", (TribeData.BonusEnum)tribeBonusAutoidx);
+			EnumCache<TribeData.BonusEnum>.AddMapping("citypark", (TribeData.BonusEnum)tribeBonusAutoidx);
         }
 
 		[HarmonyPostfix]
@@ -66,14 +73,46 @@ namespace PolyPlus {
         }
 
         [HarmonyPostfix]
+        [HarmonyPatch(typeof(MapGenerator), nameof(MapGenerator.SetTileAsCapital))]
+        private static void MapGenerator_SetTileAsCapital(GameState gameState, PlayerState playerState, TileData tile)
+	    {
+            TribeData tribeData;
+            if (tile != null && gameState.GameLogicData.TryGetData(playerState.tribe, out tribeData))
+            {
+                if (tribeData.bonus == EnumCache<TribeData.BonusEnum>.GetType("citypark"))
+                {
+                    tile.improvement.production = 2;
+                    tile.improvement.baseScore += 250;
+                    tile.improvement.AddReward(CityReward.Park);
+                }
+            }
+        }
+
+        //[HarmonyPostfix]
+        //[HarmonyPatch(typeof(PathFinderSettings), nameof(PathFinderSettings.CreateForUnit))]
+        private static void PathFinder_CreateForUnit(ref PathFinderSettings __result, UnitState unit, GameState gameState)
+	    {
+            PlayerState player;
+            if(gameState.TryGetPlayer(unit.owner, out player))
+            {
+                if(PlayerExtensions.HasAbility(player, EnumCache<PlayerAbility.Type>.GetType("waterembark"), gameState))
+                {
+                    __result.isRequiredToUsePortToGoIntoWater = false;
+                }
+            }
+        }
+
+        [HarmonyPostfix]
         [HarmonyPatch(typeof(PathFinder), nameof(PathFinder.IsTileAccessible))]
         private static void PathFinder_IsTileAccessible(ref bool __result, TileData tile, TileData origin, PathFinderSettings settings)
 	    {
             if(PlayerExtensions.HasAbility(settings.playerState, EnumCache<PlayerAbility.Type>.GetType("waterembark"), settings.gameState) && tile.IsWater && !origin.IsWater && settings.unit != null){
-                if(settings.allowedTerrain.Contains(tile.terrain) && tile.GetExplored(settings.playerState.Id)){
+                if((tile.terrain == Polytopia.Data.TerrainData.Type.Water && settings.allowedTerrain.Contains(Polytopia.Data.TerrainData.Type.Water)) || (tile.terrain == Polytopia.Data.TerrainData.Type.Ocean && settings.allowedTerrain.Contains(Polytopia.Data.TerrainData.Type.Ocean)))
+                {
                     __result = true;
                 }
-                else{
+                else
+                {
                     __result = false;
                 }
             }
@@ -92,16 +131,22 @@ namespace PolyPlus {
                 TileData tile2 = gameState.Map.GetTile(worldCoordinates);
                 tile2.SetUnit(unitState);
                 unitState.coordinates = worldCoordinates;
-                if (!unitData.IsAquatic() && !unitState.HasAbility(UnitAbility.Type.Fly, gameState) && tile2.IsWater && PlayerExtensions.HasAbility(playerState, EnumCache<PlayerAbility.Type>.GetType("waterembark"), gameState))
+                bool hasNoBridge = true;
+                if(tile2.improvement != null)
+                {
+                    if(gameState.GameLogicData.TryGetData(tile2.improvement.type, out ImprovementData imrovementData))
+                    {
+                        if(imrovementData.HasAbility(ImprovementAbility.Type.Bridge))
+                        {
+                            hasNoBridge = false;
+                        }
+                    }
+                }
+                if(hasNoBridge && !unitData.IsAquatic() && !unitState.HasAbility(UnitAbility.Type.Fly, gameState) && tile2.IsWater && PlayerExtensions.HasAbility(playerState, EnumCache<PlayerAbility.Type>.GetType("waterembark"), gameState))
                 {
                     gameState.ActionStack.Add(new EmbarkAction(__instance.PlayerId, worldCoordinates));
                 }
             }
         }
-
-        internal static string GetJTokenName(JToken token, int n = 1)
-		{
-			return token.Path.Split('.')[^n];
-		}
     }
 }
